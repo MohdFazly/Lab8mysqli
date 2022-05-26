6e08bf6f97c8fb450325d01623cae186c927b18f
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-26 15:55:17 +0800

    commit 2d)

--- add_user.php
+++ add_user.php
@@ -13,11 +13,11 @@ include 'conn.php';
 
 $sql="insert into user (first_name, last_name, email, pass_word)
 values ('$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[password]','$_POST[reg_date]')";
-    if (!mysqli_query($conn, $sql))
+    if (!mysqli_query($sql, $conn))
     {
-        echo "1 record successfully added..."; 
+        die('Error: ' .mysqli_error());
     }
-   
+    echo "1 record successfully added..."; 
 ?>
 </body>
 </html>
